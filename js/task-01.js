a90cd618e0ad976dd25e99fb83cc9058c38941af
Vasilij_Ivashkevych <vasilijivashkevich@gmail.com>
2023-02-06 09:31:04 +0100

    Завдання 1 виконано

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,17 @@
 const elementCategory = document.querySelectorAll('li.item');
-console.log(elementCategory);
 console.log('Number of categories:' , elementCategory.length);
 
 const titleCategory = document.querySelectorAll('h2');
 console.log('Category:', titleCategory[0].textContent);
 
-// const lastChildItem = elementCategory.previousSibling;
-// console.log(lastChildItem);
+const listOfCategory = document.querySelectorAll('li ul');
+const itemOfCategory = listOfCategory[0].querySelectorAll('li');
+console.log('Elements:', itemOfCategory.length);
+
+console.log('Category:', titleCategory[1].textContent);
+const itemOfCategory2 = listOfCategory[1].querySelectorAll('li');
+console.log('Elements:', itemOfCategory2.length);
+
+console.log('Category:', titleCategory[2].textContent);
+const itemOfCategory3 = listOfCategory[2].querySelectorAll('li');
+console.log('Elements:', itemOfCategory3.length);
