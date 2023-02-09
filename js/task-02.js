31601a99c2f5096deb634e949403ea35072169a8
Vasilij_Ivashkevych <vasilijivashkevich@gmail.com>
2023-02-09 01:40:34 +0100

    Виконав 1-2 завдання

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredList = document.querySelector('#ingredients');
+
+const elements = [];
+
+for (const ingredient of ingredients) {
+  const value = ingredient;
+  const ingredItem = document.createElement('li');
+  ingredItem.textContent = value;
+  elements.push(ingredItem);
+
+  console.log(ingredItem);
+}
+
+ingredList.append(...elements);
+console.log(ingredList);
