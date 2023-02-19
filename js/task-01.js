e5ff80e6d67649c3572bf808484ef1ee9b5352a5
Vasilij_Ivashkevych <vasilijivashkevich@gmail.com>
2023-02-18 23:21:10 -0600

    Update task-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,17 +1,7 @@
-const elementCategory = document.querySelectorAll('li.item');
-console.log('Number of categories:' , elementCategory.length);
+const elementCategory = document.querySelectorAll(".item");
+console.log("Number of categories:", elementCategory.length);
 
-const titleCategory = document.querySelectorAll('h2');
-console.log('Category:', titleCategory[0].textContent);
-
-const listOfCategory = document.querySelectorAll('li ul');
-const itemOfCategory = listOfCategory[0].querySelectorAll('li');
-console.log('Elements:', itemOfCategory.length);
-
-console.log('Category:', titleCategory[1].textContent);
-const itemOfCategory2 = listOfCategory[1].querySelectorAll('li');
-console.log('Elements:', itemOfCategory2.length);
-
-console.log('Category:', titleCategory[2].textContent);
-const itemOfCategory3 = listOfCategory[2].querySelectorAll('li');
-console.log('Elements:', itemOfCategory3.length);
+elementCategory.forEach(element => {
+  console.log("Category:", element.querySelector("h2").textContent);
+  console.log("Elements:", element.querySelectorAll("li").length);
+});
