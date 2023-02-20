d62fdc1ff15552ce7ab2144659bc86ce06395da0
Vasilij_Ivashkevych <vasilijivashkevich@gmail.com>
2023-02-19 23:36:22 -0600

    Update task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector(".login-form");
 
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert(`Заповніть всі поля!`);
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+
+formEl.addEventListener('submit', handleSubmit);
